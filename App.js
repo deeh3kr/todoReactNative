72a65ef9ea846b5c6d6db4eddbfb8344df4e4299
deeh3kr <deepanshu111197@gmail.com>
2020-10-18 16:36:49 +0530

    First Setup

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import Header from './components/header'
+import TodoItem from './components/todoItem'
+import AddTodo from './components/addTodo'
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    {text: 'Buy Coffee', key: 1},
+    {text: 'Create an App', key: 2},
+    {text: 'Play on the TV', key: 3},
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        {text: text, key: Math.random().toString()},
+        ...prevTodos
+      ];
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler}/>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item}) => (
+              <TodoItem item={item} pressHandler = {pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,7 +50,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content:{
+    padding: 40,
+  },
+  list:{
+    marginTop: 20,
+  },
+  header:{
+    backgroundColor:'pink',
+    padding: 20,
+  },
+  boldText:{
+    fontWeight: 'bold',
+  },
+  buttonContainer:{
+    padding:20,
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+  item:{
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+  }
 });
